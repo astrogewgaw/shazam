8fa5c80f918c71ddaf5edfa988e8032a6816ab42
Ujjwal Panda <ujjwalpanda97@gmail.com>
2025-05-26 19:22:33 +0530

    ✨ Parse all useful parameters from header.

--- src/shazam/multifrb.h
+++ src/shazam/multifrb.h
@@ -1,497 +1,245 @@
-#ifndef MULTIFRB
-#define MULTIFRB
+#ifndef MULTIFRB_H
+#define MULTIFRB_H
 
-#include <nanobind/nanobind.h>
+#include <cstring>
+#include <ctime>
+#include <memory>
+#include <stdexcept>
+#include <string>
 #include <sys/shm.h>
 #include <sys/time.h>
-#include <unistd.h>
+#include <vector>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "multihdr.h"
 
-enum { USB_130, USB_175, LSB_130, LSB_175, MAX_BANDS };
-enum { RRLL, RRRL, RR__, MACMODES };
-enum { DAS_CARD0, DAS_CARD1, DAS_CARDS, BEDAS_CARDS, GPUDAS_CARDS };
-enum {
-  IndianPolar,
-  UsbPolar,
-  LsbPolar,
-  UsbCopy,
-  LsbCopy,
-  AllU130,
-  AllU175,
-  AllL130,
-  AllL175,
-  arar_arar,
-  alal_alal,
-  brbr_brbr,
-  blbl_blbl,
-  aral_brbl,
-  aral_alar,
-  brbl_blbr,
-  arbr_albl,
-  DpcMuxVals
-};
+constexpr int FRBHDRKEY = 2031;
+constexpr int FRBBUFKEY = 2032;
 
-enum { NAMELEN = 32, DATELEN = 32 };
-
-enum {
-  MAX_ANTS = 32,
-  MAX_SAMPS = 64,
-  MAX_FFTS = MAX_SAMPS,
-  MAC_CARDS = 33,
-  MAX_BASE = DAS_CARDS * 32 * MAC_CARDS,
-  MAX_CHANS = 32768,
-  POLS = 2
-};
-
-enum { MAX_PROJECTS = 500, MAX_SCANS = 512, MAX_BM_STEER = 2568 };
-enum { MAX_ARRAYS = 1 };
-enum { MAX_BEAMS = 4 };
-enum { MAX_GATES = 16 };
-enum { LittleEndian = 1, BigEndian = 0 };
-enum { TransitObs = 32768 };
-
-enum {
-  TimeSize = sizeof(double),
-  WtSize = 2 * sizeof(float),
-  ActiveScans = 64,
-  DataFlagSize = ActiveScans * sizeof(int)
-};
-
-typedef struct interfaces {
-  char iface[30];
-} Interface;
-
-typedef struct {
-  char name[4];
-  unsigned char samp_id[MAX_BANDS];
-  double bx, by, bz; /* metres */
-  double d0[MAX_BANDS], p0[MAX_BANDS];
-} AntennaParType;
-
-typedef struct {
-  int macs, channels, pols, sta, cntrl, statime, iabeam, pabeam1, pabeam2;
-  float iabeam_res, pabeam1_res, pabeam2_res, f_step, clock;
-  unsigned char dpcmux, clksel, fftmode, macmode;
-} CorrParType;
-
-typedef struct {
-  int antmask, samplers, baselines, channels, lta, gsb_maxchan, gsb_fstop;
-  short bandmask, mode, gsb_stokes;
-  unsigned short chan_num[MAX_CHANS];
-  double mjd_ref, t_unit, gsb_acq_bw, gsb_final_bw;
-  double dut1;
-} DasParType;
-
-typedef struct {
-  char object[NAMELEN];
-  struct {
-    float i, q, u, v;
-  } flux;
-  double mjd0;
-  double ra_app, dec_app, ra_mean, dec_mean, dra, ddec;
-  double freq[2], first_lo[2], bb_lo[2];
-  double rest_freq[2], lsrvel[2];
-  double ch_width;
-  int id, net_sign[MAX_BANDS], mode, dum1;
-  unsigned int antmask;
-  unsigned short bandmask, dum2;
-  short calcode, qual;
-} SourceParType;
-
-typedef struct {
-  char object[NAMELEN], date_obs[DATELEN];
-  char observer[NAMELEN], project[NAMELEN], code[8];
-  double ra_date, dec_date, mjd_ref, dra, ddec, period;
-  int antmask, bandmask, flag, seq, fold_flg;
-  float integ, f_step, dm;
-  int rf[2], first_lo[2], bb_lo[2];
-  float BW;
-  int ref_ch, i_side_band;
-} ScanParType;
-
-typedef struct {
-  char code[8], observer[NAMELEN], title[NAMELEN];
-  unsigned int antmask;
-  unsigned short bandmask, seq;
-} ProjectType;
-
-typedef struct {
-  int status;
-  float t;
-  ProjectType proj;
-  SourceParType source;
-} ScanInfoType;
-
-typedef struct {
-  unsigned char ant_id, band, fft_id, dpc;
-} SamplerType;
-
-typedef struct {
-  SamplerType samp[2];
-  unsigned char card, chip, pol, word_incr;
-} BaseParType;
-
-typedef struct {
-  unsigned char endian, dummy[7];
-  char version[NAMELEN];
-  char bandname[MAX_BANDS][8];
-  AntennaParType antenna[MAX_ANTS];
-  SamplerType sampler[MAX_SAMPS];
-  BaseParType baseline[MAX_BASE];
-  CorrParType corrpar;
-  DasParType daspar;
-} CorrType;
-
-typedef struct {
-  unsigned int ext_model, idle, stop, userflag;
-} AntennaFlagType;
-
-typedef struct {
-  float phase, dp, delay, dd;
-} ModelParType;
-typedef struct {
-  float phase, dp, delay, dslope;
-} DataParType;
-
-typedef struct {
-  double t0;
-  int ant_id, band;
-  ModelParType par;
-} ExtModelType;
-
-typedef struct {
-  int BitPackFlag;
-  int DataBitFormat;
-  int DataLSBBits;
-  int DataBitWinSize;
-} BitPackType;
-
-typedef struct {
-  int FoldingFlag;
-  double FoldingPeriod;
-} FoldingType;
-
-typedef struct {
-  int BBRFIFilterFlag;
-  char ReplacementType[64];
-  int ReplaceValue;
-  float Threshold;
-  char Strategy[32];
-  char param1[64];
-  char param2[64];
-  char param3[64];
-} BBRFIFilterType;
-
-typedef struct {
-  int BandEqFlag;
-  float optlevel;
-  int beg_chan;
-  int end_chan;
-  float rollOff;
-  int taps;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-} BandEqType;
-
-typedef struct {
-  int BeamSteeringFlag;
-  int nPCBaselines;
-  int nBeamHosts;
-  int nSteeringBeams;
-  short int nSteeringBeamsPerNode;
-  float RA[MAX_BM_STEER];
-  float DEC[MAX_BM_STEER];
-  int Beam_index[MAX_BM_STEER];
-  int Beam_subindex[MAX_BM_STEER];
-  char param2[64];
-  char param3[64];
-  char param4[64];
-} BeamSteeringType;
-
-typedef struct {
-  char Feature1Title[64];
-  int Feature1Flag;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-  char param4[64];
-} Feature1Type;
-
-typedef struct {
-  char Feature2Title[64];
-  int Feature2Flag;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-  char param4[64];
-} Feature2Type;
-
-typedef struct {
-  char Feature3Title[64];
-  int Feature3Flag;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-  char param4[64];
-} Feature3Type;
-
-typedef struct {
-  char Feature4Title[64];
-  int Feature4Flag;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-  char param4[64];
-} Feature4Type;
-
-typedef struct {
-  int WalshFlag;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-} WalshType;
-
-typedef struct {
-  int PFBFlag;
-  int Taps;
-  char param1[64];
-  char param2[64];
-  char param3[64];
-} PFBType;
-
-typedef struct {
-  int CDFlag;
-  unsigned int in_channels;
-  unsigned int out_subbands;
-  float freq;
-  float bw;
-  float DM;
-  int sb_flag;
-  int vlt_bits;
-  int write_int;
-  float sampling_resolution;
-} CDType;
-
-typedef struct {
-  int DedispersionFlag;
-  double dD_RF;
-  double dD_DM;
-  double dD_BW;
-  int dD_RefChan;
-  int dD_iSB;
-} dDType;
-
-typedef struct {
-  int GatingFlag;
-  int NoOfGates;
-  int GtPeriod;
-  int GtNoOfBins;
-  int GtStartBinNo[MAX_GATES];
-  int GtStopBinNo[MAX_GATES];
-} GatingType;
-
-typedef struct {
-  int FilterFlag;
-  int FftLength;
-  int NBands;
-  int FilterDataType;
-} RfiFilterType;
-
-typedef struct {
-  int BeamHostID;
-  char BeamHostName[128];
-  int PreTimeInt;
-  double SampInterval;
-  int GAC_maskP1;
-  int GAC_maskP2;
-  unsigned long GAC_maskFp[MAX_BEAMS];
-  int GAC_vlt_maskP1[MAX_BEAMS];
-  int GAC_vlt_maskP2[MAX_BEAMS];
-  int PostFreqInt[MAX_BEAMS];
-  int PostTimeInt[MAX_BEAMS];
-  double PrePeriod[MAX_BEAMS];
-  float DM[MAX_BEAMS];
-  int BeamType[MAX_BEAMS];
-  int NStokes[MAX_BEAMS];
-  int OutputDataFormat;
-  BitPackType BitPackParams;
-  dDType dDParams;
-  CDType CDParams;
-  FoldingType FoldingParams;
-  GatingType GatingParams;
-  RfiFilterType RfiFilterParams;
-  PFBType PFBparams;
-  WalshType WalshParams;
-  BandEqType BandEqParams;
-  BBRFIFilterType BBRFIFilterParams;
-  BeamSteeringType BeamSteeringParams;
-  Feature1Type Feature1Params;
-  Feature2Type Feature2Params;
-  Feature3Type Feature3Params;
-  Feature4Type Feature4Params;
-} BeamGenType;
-
-typedef struct {
-  short int SyncCmd;
-  short int VersionMajor;
-  short int VersionMinor;
-  ProjectType Project[MAX_PROJECTS];
-  ScanInfoType ScanTab[MAX_SCANS];
-  ScanInfoType ScanTab1[MAX_SCANS];
-  ScanInfoType ScanTab2[MAX_SCANS];
-  ScanInfoType ScanTab3[MAX_SCANS];
-  CorrType corr;
-  ScanParType scanpar[MAX_ARRAYS];
-  BeamGenType BeamGenHdr;
-} BeamHeaderType;
-
-#define AntennaTypeSize sizeof(AntennaType)
-#define MacFftTypeSize sizeof(MacFftType)
-#define SamplerTypeSize sizeof(SamplerType)
-#define DataParTypeSize sizeof(DataParType)
-#define CorrTypeSize sizeof(CorrType)
-#define CorrSize sizeof(CorrType)
-#define Corr2Size 16192
-
-typedef struct {
-  int in0, in1, out0, out1;
-} IndexType;
-
-typedef struct {
-  double bxcd, bycd, bzsd, fixed, phase, bb_lo, freq, f_step;
-} GeomType;
-
-#define DAS_H_KEY 1030
-#define DAS_D_KEY 1031
-#define DAS_H0_KEY 1032
-#define DAS_D0_KEY 1033
-#define DAS_HDRSIZE 200000
-#define DAS_BUFSIZE 545259520
-
-typedef struct {
-  int s0, s1, card;
-  int delay, p0, pstep0, p1, pstep1, fstc;
-  float p2fstc, fstc_step;
-} FftDelayParType;
-
-typedef struct {
-  double clock, t_update;
-  double pc_time;
-  double t_off;
-  double delay_step, fstc_scale, nco_tick_time;
-  int cycle, seq, cards;
-  unsigned char dpcmux, clksel, fftmode, macmode;
-  ModelParType par[MAX_SAMPS];
-  FftDelayParType fdpar[MAX_SAMPS / 2];
-} ModelInfoType;
-
-typedef struct {
-  int active, status, scan, scan_off;
-  CorrType corr;
-  ModelInfoType model;
-  BeamHeaderType BeamHeader;
-  char buf[DAS_HDRSIZE];
-} DasHdrType;
-
-enum {
-  BufMarked = 1,
-  BufReady = 1 << 1,
-  Rack0Bad = 1 << 2,
-  Rack1Bad = 1 << 3,
-  Rack2Bad = 1 << 4,
-  Rack3Bad = 1 << 5,
-  Rack4Bad = 1 << 6,
-  Rack5Bad = 1 << 7,
-  BufFinish = 1 << 8,
-  MaxDataBuf = 100
-};
-
-enum { MAX_EVENTS = 50000 };
-
-typedef struct {
-  float t;
-  unsigned char type, cmd;
-  unsigned short seq;
-  int flag_num, scan_num;
-} EventLogType;
-
-typedef struct {
-  int t_off, wt_off, par_off, data_off, data_words;
-  int par_words, wordsize;
-} RecOffsetType;
-
-typedef struct {
-  int off;
-  BaseParType base;
-  char name[12];
-} MacWordType;
-
-typedef struct {
-  unsigned char seq, scan;
-  int status, recl, seqnum;
-  RecOffsetType off;
-  MacWordType *mac;
-  char *buf;
-} RecBufType;
-
-typedef struct {
-  int active, status;
-  unsigned short events, flags, starts, stops;
-  CorrType corr;
-  AntennaFlagType flag[MAX_EVENTS][MAX_BANDS];
-  EventLogType event[MAX_EVENTS];
-  ScanInfoType scaninfo[MAX_SCANS];
-  RecOffsetType offset;
-} DataInfoType;
-
-typedef struct {
-  int flag, rec, seqnum;
-  unsigned short flag_seq, newstate;
-} DataTabType;
-
-typedef struct {
-  int flag, blocksize, maxblocks, cur_block, first_block, cur_rec;
-  DataTabType dtab[MaxDataBuf];
-  char buf[DAS_BUFSIZE];
-} DataBufType;
-
-#define CHANNEL 4096
-#define FFTLEN (2 * CHANNEL)
-#define NCHAN 64
-#define CORRLEN (NCHAN * FFTLEN)
-#define NUM_ANT 1
-#define NUM_ACQ 1
-
-#define BandWidth 400000000
-#define BLOCKTIME (8 / 32)
-#define SAMP_CLK 0.000000005
-
-#ifdef __cplusplus
-}
-#endif
-
-constexpr int HDRKEY = 2031;
-constexpr int BUFKEY = 2032;
-
-constexpr int BCNTBLKS = 4;
-constexpr int MAXBLKS = 12;
 constexpr int FFTSAMPS = 800;
-constexpr int BLKSAMPS = FFTSAMPS * 32;
+constexpr int FRBMAXBLKS = 12;
+constexpr int FRBBLKSAMPS = FFTSAMPS * 32;
+
+constexpr char ANTLIST[30][4] = {
+    "C00", "C01", "C02", "C03", "C04", "C05", "C06", "C08", "C09", "C10",
+    "C11", "C12", "C13", "C14", "E02", "E03", "E04", "E05", "E06", "S01",
+    "S02", "S03", "S04", "S06", "W01", "W02", "W03", "W04", "W05", "W06"};
+
+constexpr char BEAMTYPES[7][6] = {"IA",  "PA",   "VLT", "PC",
+                                  "CDP", "PASV", "MISC"};
 
 typedef struct {
-  unsigned int active, status, isempty;
-  double pctime, reftime, rectime;
-  struct timeval timestamps[MAXBLKS];
-  double nanoseconds[MAXBLKS];
-  unsigned int flag, curblk, currec, blksize, nbeams;
+  unsigned int active;
+  unsigned int status;
+  unsigned int empty;
+  double pctime;
+  double reftime;
+  double rectime;
+  struct timeval timestamps[FRBMAXBLKS];
+  double nanoseconds[FRBMAXBLKS];
+  unsigned int flag;
+  unsigned int curblk;
+  unsigned int currec;
+  unsigned int blksize;
+  unsigned int nbeams;
   int overflow;
 } BeamBufferType;
 
-namespace nb = nanobind;
-void init_multifrb(nb::module_ m);
+template <typename... Args>
+std::string safe_strfmt(const std::string &format, Args... args) {
+  int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;
+  if (size_s <= 0) throw std::runtime_error("Error during formatting.");
+  auto size = static_cast<size_t>(size_s);
+  std::unique_ptr<char[]> buf(new char[size]);
+  std::snprintf(buf.get(), size, format.c_str(), args...);
+  return std::string(buf.get(), buf.get() + size - 1);
+}
+
+inline std::string safe_strftime(const char *fmt, const std::tm *t) {
+  std::size_t len = sizeof(fmt);
+  auto buff = std::make_unique<char[]>(len);
+  while (std::strftime(buff.get(), len, fmt, t) == 0) {
+    len *= 2;
+    buff = std::make_unique<char[]>(len);
+  }
+  return std::string{buff.get()};
+}
+
+class MultiFRBSHM {
+public:
+  MultiFRBSHM() {
+    /** Attach to header. **/
+    int HDRID = shmget(FRBHDRKEY, sizeof(BeamHeaderType), SHM_RDONLY);
+    if (HDRID < 0) throw std::runtime_error("");
+    hdrptr = (BeamHeaderType *)shmat(HDRID, 0, SHM_RDONLY);
+    if ((void *)hdrptr == (void *)-1) throw std::runtime_error("");
+
+    /** Read in all header parameters... **/
+    ScanInfoType *scan = &(hdrptr->ScanTab[0]);
+
+    /** Get some beam and host parameters early. **/
+    m_beamid = hdrptr->BeamGenHdr.BeamHostID;
+    m_hostid = hdrptr->BeamGenHdr.BeamHostID;
+    m_hostname = hdrptr->BeamGenHdr.BeamHostName;
+
+    /** Get data parameters. **/
+    m_nbits = 8;
+    m_nf = hdrptr->corr.corrpar.channels;
+    m_fh = scan->source.freq[0] / 1e6;
+    m_df = hdrptr->corr.corrpar.f_step / 1e6;
+    m_flipped = scan->source.net_sign[0] == -1;
+    m_dt = hdrptr->corr.daspar.gsb_final_bw * hdrptr->BeamGenHdr.SampInterval /
+           (hdrptr->corr.corrpar.clock);
+
+    /** Some derived parameters. **/
+    m_bw = m_nf * m_df;
+    if (m_flipped) m_fh = m_fh + m_bw - 0.5 * m_df;
+    m_fl = m_fh - m_bw + 0.5 * m_df;
+
+    /** Get observation parameters. **/
+    m_ra = scan->source.ra_app;
+    m_dec = scan->source.dec_app;
+    m_gtaccode = scan->proj.code;
+    m_source = scan->source.object;
+    m_gtactitle = scan->proj.title;
+    m_observer = scan->proj.observer;
+    m_nstokes = hdrptr->BeamGenHdr.NStokes[m_beamid];
+    m_beammode = BEAMTYPES[hdrptr->BeamGenHdr.BeamType[m_beamid] - 1];
+
+    /** Get antennas mask and antennas. **/
+    unsigned int refantmask = 1;
+    m_antmaskpol1 = hdrptr->BeamGenHdr.GAC_maskP1;
+    for (int i = 0; i < 30; i++)
+      if ((refantmask << i) & m_antmaskpol1) m_antspol1.push_back(ANTLIST[i]);
+    m_antmaskpol2 = hdrptr->BeamGenHdr.GAC_maskP2;
+    for (int i = 0; i < 30; i++)
+      if ((refantmask << i) & m_antmaskpol2) m_antspol2.push_back(ANTLIST[i]);
+
+    /** Get beam steering parameters. **/
+    m_nbeams = hdrptr->BeamGenHdr.BeamSteeringParams.nSteeringBeams;
+    m_npcbaselines = hdrptr->BeamGenHdr.BeamSteeringParams.nPCBaselines;
+    m_nbeamspernode =
+        hdrptr->BeamGenHdr.BeamSteeringParams.nSteeringBeamsPerNode;
+
+    /** Get beam RA and DEC values. **/
+    for (int i = 0; i < m_nbeamspernode; i++) {
+      int b = m_beamid * m_nbeamspernode + i;
+      m_beamras.push_back(hdrptr->BeamGenHdr.BeamSteeringParams.RA[b]);
+      m_beamdecs.push_back(hdrptr->BeamGenHdr.BeamSteeringParams.DEC[b]);
+    }
+
+    /** Calculate size of buffer. **/
+    long BLKSIZE = (long)FRBBLKSAMPS * (long)m_nf;
+    long BUFSIZE = BLKSIZE * (long)FRBMAXBLKS * (long)m_nbeamspernode;
+    long FRBSHMSIZE = sizeof(BeamBufferType) + BUFSIZE;
+
+    /** Attach to buffer. **/
+    int BUFID = shmget(FRBBUFKEY, FRBSHMSIZE, SHM_RDONLY);
+    if (BUFID < 0) throw std::runtime_error("");
+    bufptr = (BeamBufferType *)shmat(BUFID, 0, SHM_RDONLY);
+    if ((void *)bufptr == (void *)-1) throw std::runtime_error("");
+    dataptr = ((unsigned char *)bufptr) + sizeof(BeamBufferType);
+
+    /** Get index of current record and block being written. **/
+    if (bufptr->empty) {
+      curblk = bufptr->curblk;
+      currec = bufptr->currec;
+    } else {
+      curblk = bufptr->curblk;
+      currec = (bufptr->currec - 1) % FRBMAXBLKS;
+    }
+
+    /** Get date and time of observation. **/
+    double seconds;
+    struct tm *localt;
+    double nanoseconds = 0;
+    struct timeval timestamp;
+
+    memcpy(&timestamp, &(bufptr->timestamps[currec]), sizeof(struct timeval));
+    memcpy(&nanoseconds, &(bufptr->nanoseconds[currec]), sizeof(nanoseconds));
+
+    localt = localtime(&timestamp.tv_sec);
+    seconds = localt->tm_sec;
+    seconds += (timestamp.tv_usec) / 1e6;
+    seconds += (nanoseconds) / 1e9;
+
+    m_obsdate = safe_strftime("%d/%m/%Y", localt);
+    m_obstime = safe_strfmt("%02d:%02d:%012.9lf", localt->tm_hour,
+                            localt->tm_min, seconds);
+  };
+
+  ~MultiFRBSHM() {};
+
+  int nf() { return m_nf; };
+  double fh() { return m_fh; };
+  double fl() { return m_fl; };
+  double df() { return m_df; };
+  double bw() { return m_bw; };
+  double dt() { return m_dt; };
+  int nbits() { return m_nbits; };
+  int nstokes() { return m_nstokes; };
+  bool flipped() { return m_flipped; };
+
+  double ra() { return m_ra; };
+  double dec() { return m_dec; };
+  int beamid() { return m_beamid; };
+  int hostid() { return m_hostid; };
+  int nbeams() { return m_nbeams; };
+  std::string source() { return m_source; };
+  std::string obsdate() { return m_obsdate; };
+  std::string obstime() { return m_obstime; };
+  int npcbaselines() { return m_npcbaselines; };
+  int nbeamspernode() { return m_nbeamspernode; };
+  std::string hostname() { return m_hostname; };
+  std::string beammode() { return m_beammode; };
+  std::string observer() { return m_observer; };
+  std::string gtaccode() { return m_gtaccode; };
+  std::string gtactitle() { return m_gtactitle; };
+  unsigned int antmaskpol1() { return m_antmaskpol1; };
+  unsigned int antmaskpol2() { return m_antmaskpol2; };
+  std::vector<double> beamras() { return m_beamras; };
+  std::vector<double> beamdecs() { return m_beamdecs; };
+  std::vector<std::string> antspol1() { return m_antspol1; };
+  std::vector<std::string> antspol2() { return m_antspol2; };
+
+private:
+  int m_nf;
+  int m_nbits;
+  double m_fh;
+  double m_fl;
+  double m_df;
+  double m_bw;
+  double m_dt;
+  double m_mjd;
+  int m_nstokes;
+  bool m_flipped;
+
+  double m_ra;
+  double m_dec;
+  int m_beamid;
+  int m_hostid;
+  int m_nbeams;
+  int m_npcbaselines;
+  int m_nbeamspernode;
+  std::string m_obsdate;
+  std::string m_obstime;
+  std::string m_source;
+  std::string m_hostname;
+  std::string m_beammode;
+  std::string m_observer;
+  std::string m_gtaccode;
+  std::string m_gtactitle;
+  unsigned int m_antmaskpol1;
+  unsigned int m_antmaskpol2;
+  std::vector<double> m_beamras;
+  std::vector<double> m_beamdecs;
+  std::vector<std::string> m_antspol1;
+  std::vector<std::string> m_antspol2;
+
+  int currec;
+  int curblk;
+  BeamHeaderType *hdrptr;
+  BeamBufferType *bufptr;
+  unsigned char *dataptr;
+};
 
 #endif
--- src/shazam/shazam.C
+++ src/shazam/shazam.C
@@ -1,6 +1,77 @@
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
 
 #include "multifrb.h"
-#include "shazam.h"
 
-NB_MODULE(_internals, m) { init_multifrb(m); }
+namespace nb = nanobind;
+using MFS = MultiFRBSHM;
+
+NB_MODULE(internals, m) {
+  nb::class_<MFS>(m, "MultiFRBSHM")
+      .def(nb::init<>())
+      .def_prop_ro("nf", [](MFS &x) { return x.nf(); })
+      .def_prop_ro("fh", [](MFS &x) { return x.fh(); })
+      .def_prop_ro("fl", [](MFS &x) { return x.fl(); })
+      .def_prop_ro("df", [](MFS &x) { return x.df(); })
+      .def_prop_ro("bw", [](MFS &x) { return x.bw(); })
+      .def_prop_ro("dt", [](MFS &x) { return x.dt(); })
+      .def_prop_ro("ra", [](MFS &x) { return x.ra(); })
+      .def_prop_ro("dec", [](MFS &x) { return x.dec(); })
+      .def_prop_ro("nbits", [](MFS &x) { return x.nbits(); })
+      .def_prop_ro("beamid", [](MFS &x) { return x.beamid(); })
+      .def_prop_ro("hostid", [](MFS &x) { return x.hostid(); })
+      .def_prop_ro("nbeams", [](MFS &x) { return x.nbeams(); })
+      .def_prop_ro("nstokes", [](MFS &x) { return x.nstokes(); })
+      .def_prop_ro("flipped", [](MFS &x) { return x.flipped(); })
+      .def_prop_ro("source", [](MFS &x) { return x.source(); })
+      .def_prop_ro("obsdate", [](MFS &x) { return x.obsdate(); })
+      .def_prop_ro("obstime", [](MFS &x) { return x.obstime(); })
+      .def_prop_ro("hostname", [](MFS &x) { return x.hostname(); })
+      .def_prop_ro("beammode", [](MFS &x) { return x.beammode(); })
+      .def_prop_ro("observer", [](MFS &x) { return x.observer(); })
+      .def_prop_ro("npcbaselines", [](MFS &x) { return x.npcbaselines(); })
+      .def_prop_ro("gtaccode", [](MFS &x) { return x.gtaccode(); })
+      .def_prop_ro("gtactitle", [](MFS &x) { return x.gtactitle(); })
+      .def_prop_ro("nbeamspernode", [](MFS &x) { return x.nbeamspernode(); })
+      .def_prop_ro("beamras", [](MFS &x) { return x.beamras(); })
+      .def_prop_ro("antmaskpol1", [](MFS &x) { return x.antmaskpol1(); })
+      .def_prop_ro("antmaskpol2", [](MFS &x) { return x.antmaskpol2(); })
+      .def_prop_ro("beamdecs", [](MFS &x) { return x.beamdecs(); })
+      .def_prop_ro("antspol1", [](MFS &x) { return x.antspol1(); })
+      .def_prop_ro("antspol2", [](MFS &x) { return x.antspol2(); })
+      .def_prop_ro("header", [](MFS &x) {
+        nb::dict header;
+        header["nf"] = x.nf();
+        header["fh"] = x.fh();
+        header["fl"] = x.fl();
+        header["df"] = x.df();
+        header["bw"] = x.bw();
+        header["dt"] = x.dt();
+        header["ra"] = x.ra();
+        header["dec"] = x.dec();
+        header["nbits"] = x.nbits();
+        header["beamid"] = x.beamid();
+        header["hostid"] = x.hostid();
+        header["nbeams"] = x.nbeams();
+        header["source"] = x.source();
+        header["nstokes"] = x.nstokes();
+        header["flipped"] = x.flipped();
+        header["obsdate"] = x.obsdate();
+        header["obstime"] = x.obstime();
+        header["beamras"] = x.beamras();
+        header["beamdecs"] = x.beamdecs();
+        header["hostname"] = x.hostname();
+        header["beammode"] = x.beammode();
+        header["observer"] = x.observer();
+        header["antspol1"] = x.antspol1();
+        header["antspol2"] = x.antspol2();
+        header["gtaccode"] = x.gtaccode();
+        header["gtactitle"] = x.gtactitle();
+        header["antmaskpol1"] = x.antmaskpol1();
+        header["antmaskpol2"] = x.antmaskpol2();
+        header["npcbaselines"] = x.npcbaselines();
+        header["nbeamspernode"] = x.nbeamspernode();
+        return header;
+      });
+}
